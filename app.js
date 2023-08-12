33a48dccca27d0bbb955f9bbc04d20605125e0d3
hongandepzzi <94431987+hongandepzzi@users.noreply.github.com>
2023-08-12 20:18:49 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+alert("Chào mừng đến trang web của ân\nÈn gioi😛")
+function myFunction() {
+    let kg = prompt("Bố này nặng bao nhiêu kg");
+    if (kg != null) {
+        document.getElementById("demi").innerHTML =
+        "Bo may beo vl"
+    }
+}
+function muFunction() {
+    var txt;
+    if (confirm("Bấm ok đi (If you got some bolls)")) {
+        txt = "thì ra mẹ mày béo";
+    }
+    else {
+        txt = "Thì ra mày ko có bolls";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
+function mnFunction() {
+    window.print();
+}
+function miFunction() {
+    alert("sao may beo vay tran=)))")
+}
+function mbFunction() {
+    alert("Bấm vào cái hình á")
+}
+function mcFunction() {
+    alert("Thấy đẹp trai quá mới xin in4 đúng không=)))")
+}
+let name = "An"
+console.log("Hello" + " " + name)
+console.log("Hello An may co khoe khong ")
+
+
+
+
+
+
+
+
